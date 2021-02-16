bbaacab021882167577404fda1681b4850d653a7
Saumya Darne <darnesaumya@gmail.com>
2021-02-16 22:33:49 +0530

    made some basic changes and setup to speed up future process
    Populated table from js, probably a better way is possible, will look into it tomorrow, and also do some basic responsive designing tomorrow

--- controller/test.js
+++ controller/test.js
@@ -1,28 +1,72 @@
 window.onload = function () {
-    var testOptionsM = [{
-        "text": "IAS Officer",
-    },
-    {
-        "text": "IAS Officer",
-    },
-    {
-        "text": "IAS Officer",
-    },
-    {
-        "text": "IAS Officer",
-    }];
-
     var tbody = document.getElementById("testTable").getElementsByTagName('tbody')[0];
-    //Insert new row
-    var newRow = tbody.insertRow();
-
     for (var i of testOptionsM) {
-        // Insert a cell at the end of the row
-        var newCell = newRow.insertCell();
-        // Append a text node to the cell
-        var newText = document.createTextNode(i.text);
-        newCell.appendChild(newText);
-        console.log(i);
+        var newRow = tbody.insertRow();
+        newRow.className = "rowClass";
+        for (var j =0; j<i.Row.length-1; j++) {
+            var newCell = document.createElement("TD");
+            newCell.className = "tdClass";
+            var select1 = document.createElement("input");
+            select1.setAttribute("type","checkbox");
+            var text1 = document.createTextNode(i.Row[j].occ);    
+            var select2 = document.createElement("input");
+            select2.setAttribute("type","checkbox");
+            var text2 = document.createTextNode(i.Row[j+1].occ);    
+            newCell.appendChild(select1);
+            newCell.appendChild(text1);
+            newCell.appendChild(select2);
+            newCell.appendChild(text2);
+            newRow.appendChild(newCell);          
+        }
     }
-
 }
+var testOptionsM = [
+    //Row 1
+    {
+        Row: [{
+                type: "Adm. 1",
+                occ: "I.A.S Officer",
+                selected: false
+            },
+            {
+                type: "Adm. 2",
+                occ: "Diplomatic Services",
+                selected: false
+            },
+            {
+                type: "Ent. 1",
+                occ: "Sales Manager",
+                selected: false
+            },
+            {
+                type: "Adm. 2",
+                occ: "Factory Manager",
+                selected: false
+            }
+        ]
+    },
+    //Row 2
+    {
+        Row: [{
+                type: "Adm. 1",
+                occ: "Executive Manager",
+                selected: false
+            },
+            {
+                type: "Ent. 2",
+                occ: "Automobile Dealer",
+                selected: false
+            },
+            {
+                type: "Ent. 1",
+                occ: "Airplane Pilot",
+                selected: false
+            },
+            {
+                type: "Ent. 2",
+                occ: "Exporter",
+                selected: false
+            }
+        ]
+    }
+];
